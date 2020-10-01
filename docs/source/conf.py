9ef79c9835b24eb6a70d06f3c52ebbbbee39f137
Johannes Tochtermann <Johannes.Tochtermann@campus.lmu.de>
2020-10-01 19:24:25 +0200

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
-autodock_mock_imports = ['dm-reverb']
+autodock_mock_imports = ['reverb']
 
 
 # -- Project information -----------------------------------------------------
